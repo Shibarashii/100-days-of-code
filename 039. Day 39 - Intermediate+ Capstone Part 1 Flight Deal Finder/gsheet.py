092d179b1f8758659f7cdd990553a7cb65582ff6
Shiba <seanrei.ethan23@gmail.com>
2025-12-11 00:18:43 +0800

    successfully filled IATA Codes in Google Sheet using the Amadeus City Search API

--- 039. Day 39 - Intermediate+ Capstone Part 1 Flight Deal Finder/gsheet.py
+++ 039. Day 39 - Intermediate+ Capstone Part 1 Flight Deal Finder/gsheet.py
@@ -32,7 +32,7 @@ class GSheetClient:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    root/"credentials.json", self.scopes
+                    root/"google.credentials.json", self.scopes
                 )
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
--- 039. Day 39 - Intermediate+ Capstone Part 1 Flight Deal Finder/main.py
+++ 039. Day 39 - Intermediate+ Capstone Part 1 Flight Deal Finder/main.py
@@ -2,12 +2,18 @@ import requests
 from pathlib import Path
 from dotenv import load_dotenv
 from gsheet import GSheetClient
+from amadeus import AmadeusClient
+import json
 import os
 
 load_dotenv()
+root = Path(__file__).parent
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 full_range = "A:C"
+
+amadeus = AmadeusClient()
+gsheet = GSheetClient(SPREADSHEET_ID, SCOPES)
 
 
 def print_rows(rows):
@@ -15,12 +21,31 @@ def print_rows(rows):
         print(row)
 
 
-def main():
-    gsheet = GSheetClient(SPREADSHEET_ID, SCOPES)
-    results = gsheet.get_values(full_range)
-    rows = results["values"]  # type: ignore
+def save_json(file):
+    with open(root/"temp.json", "w") as f:
+        json.dump(obj=file, fp=f, indent=2)
 
-    print_rows(rows)
+
+def fill_iata_columns():
+    cities = gsheet.get_values("A2:A")["values"]  # type: ignore
+    iata_codes = []
+    for city in cities:
+        response = amadeus.city_search(city[0], max=3).json()
+        data = response["data"][0]
+        iata_code = data["iataCode"]
+        iata_codes.append([iata_code])
+
+    result = gsheet.update_values("B2:B", iata_codes, "USER_ENTERED")
+    if result:
+        print("Successfully updated IATA Codes in Google Sheets.")
+
+
+def main():
+    # response = amadeus.city_search("paris", max=5).json()
+    # data = response["data"][0]
+    # iata_code = data["iataCode"]
+
+    return
 
 
 if __name__ == "__main__":
